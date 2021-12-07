9f1754dc69119e65fa1d91c38eac0350fc77e8c9
mjsamuel <matthew.samuel@outlook.com.au>
2021-12-07 13:34:03 +1100

    center column and playing around with tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,30 @@ const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
+
+      {/* <ul class="flex">
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+        </li>
+      </ul> */}
+
+      {/* <div class="grid grid-cols-3 gap-4">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div> */}
       <h1 class="font-bold text-green">matt.</h1>
+
     </main>
   )
 }
