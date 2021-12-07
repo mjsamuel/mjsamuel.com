b40e10de5aa41ae64df3951f416639b00e79b999
mjsamuel <matthew.samuel@outlook.com.au>
2021-12-07 13:44:35 +1100

    setup basic projects component

---
+++ src/components/projects.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+// markup
+const Projects = () => {
+  return (
+    <div class="grid grid-cols-3 gap-4 text-center">
+      <div class="bg-green text-white">1</div>
+      <div class="bg-green text-white">2</div>
+      <div class="bg-green text-white">3</div>
+      <div class="bg-green text-white">4</div>
+    </div>
+  )
+}
+
+export default Projects
