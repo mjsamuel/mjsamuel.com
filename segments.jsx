e20e8693025b622c3c604ea98c971bfd5761fea8
mjsamuel <matthew.samuel@outlook.com.au>
2021-12-14 23:29:42 +1100

    add new components

---
+++ segments.jsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Projects from "../components/projects"
+
+// markup
+const Segments = () => {
+    return (
+        <></>
+    )
+}
+
+export default Segments
---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+// markup
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li>
+          <button class="g-transparent hover:bg-yellow text-yellow font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Resume
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
