b5a515285dbe8ac508480954a94128c1603fa30b
mjsamuel <matthew.samuel@outlook.com.au>
2021-12-22 23:47:13 +1100

    added basic side panel

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,9 +8,8 @@ const IndexPage = () => {
   return (
     <main className="h-screen p-12">
       <title>Home Page</title>
-      <div className="md:grid grid-cols-1 md:grid-cols-2 h-full">
+      <div className="md:grid gap-8 grid-cols-2 h-full">
 
-        {/*  */}
         <div className="order-2 md:order-1 h-1/5 md:h-full">
           <ExampleComponent
             background="#E4E2DC"
@@ -18,11 +17,15 @@ const IndexPage = () => {
           />
         </div>
 
-        {/*  */}
-        <div className="text-right order-1 md:order-2 h-auto">
-          <h1 className="font-bold text-green animate-fade-in-up text-xl">
-            MATTHEW SAMUEL
+        <div className="order-1 md:order-2 flex flex-col justify-end text-green">
+          <h1 className="text-6xl text-right font-bold flex-grow animate-fade-in-up ">
+            MATTHEW<br/>SAMUEL
           </h1>
+          <div className="mb-2 font-medium">
+            <h2 className="border-b-2 border-t-2 text-xl py-1">ABOUT</h2>
+            <h2 className="border-b-2 text-xl py-1">WORK</h2>
+            <h2 className="border-b-2 text-xl py-1">CONTACT ME</h2>
+          </div>
         </div>
 
       </div>
