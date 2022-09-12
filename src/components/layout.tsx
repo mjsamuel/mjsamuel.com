7c6debd98943d824efaab117e20aa35455356988
mjsamuel <matthew.samuel@outlook.com.au>
2022-09-12 21:38:00 +1000

    refactoring

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Helmet } from "react-helmet";
 import { tColors } from "../shared";
 
-const Layout = ({children, title}) => {
+const Layout = ({children, title}: { children: any, title: string }) => {
   return (
     <>
       <Helmet>
@@ -10,7 +10,7 @@ const Layout = ({children, title}) => {
         <meta name="theme-color" content={tColors.white} />
       </Helmet>
       <main className="flex justify-center text-green">
-        <div className="w-full max-w-screen-lg">{children}</div>
+        <div className="w-full max-w-screen-md">{children}</div>
       </main>
     </>
   );
